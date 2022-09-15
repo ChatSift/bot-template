d4c805d9c2030543a9ef734aa72389a08b086265
Micah Benac <micahbenac52@gmail.com>
2022-09-15 17:01:40 -0400

    additional linting changes

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,6 @@
-import '../index';
-import { expect, test } from 'vitest';
+import "../index.js";
+import { expect, test } from "vitest";
 
-test('something!', () => {
-	expect('something').toBe('something');
+test("something!", () => {
+	expect("something").toBe("something");
 });
